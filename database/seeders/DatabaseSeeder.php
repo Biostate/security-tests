7bce51a047b6ca75e84b8e6edf83c88a79ff139e
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2023-05-04 21:49:05 +0300

    updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
+        \App\Models\User::factory()->hasTodos(10)->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'is_active' => true,
